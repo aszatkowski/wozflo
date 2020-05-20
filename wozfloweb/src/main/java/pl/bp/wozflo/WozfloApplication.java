0652aba4b5d1a482296cfae3bd37989cc43d8706
aszatkowski <szatkowski.artur@gmail.com>
2020-05-20 21:45:13 +0200

    ADD: dodanie projektu webowego typu spring.

---
+++ wozfloweb/src/main/java/pl/bp/wozflo/WozfloApplication.java
@@ -0,0 +1,13 @@
+package pl.bp.wozflo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WozfloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WozfloApplication.class, args);
+	}
+
+}
---
+++ wozfloweb/src/test/java/pl/bp/wozflo/WozfloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bp.wozflo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WozfloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
